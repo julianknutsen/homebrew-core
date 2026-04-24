6372d3d1b2c6cc4111eebe32dc5488465106e21a
Thierry Moisan <thierry.moisan@gmail.com>
2026-04-24 11:54:18 -0400

    twitch-cli: add shell completions

--- Formula/t/twitch-cli.rb
+++ Formula/t/twitch-cli.rb
@@ -21,6 +21,7 @@ class TwitchCli < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.buildVersion=#{version}", output: bin/"twitch")
+    generate_completions_from_executable(bin/"twitch", shell_parameter_format: :cobra)
   end
 
   test do
