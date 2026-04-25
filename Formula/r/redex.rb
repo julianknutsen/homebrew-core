5f8729aee9cb469df22318d95a34f986299376c8
botantony <antonsm21@gmail.com>
2026-04-25 02:04:35 +0200

    redex: revision bump (boost 1.91.0)

--- Formula/r/redex.rb
+++ Formula/r/redex.rb
@@ -5,6 +5,7 @@ class Redex < Formula
   desc "Bytecode optimizer for Android apps"
   homepage "https://fbredex.com/"
   license "MIT"
+  revision 1
   head "https://github.com/facebook/redex.git", branch: "main"
 
   stable do
