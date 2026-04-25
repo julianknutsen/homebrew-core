4d541ef28c544267ca2ef4f5cdadceb07c42837c
botantony <antonsm21@gmail.com>
2026-04-25 02:05:14 +0200

    eigenpy: revision bump (boost-python3 1.91.0)

--- Formula/e/eigenpy.rb
+++ Formula/e/eigenpy.rb
@@ -2,7 +2,7 @@ class Eigenpy < Formula
   desc "Python bindings of Eigen library with Numpy support"
   homepage "https://github.com/stack-of-tasks/eigenpy"
   license "BSD-2-Clause"
-  revision 4
+  revision 5
   head "https://github.com/stack-of-tasks/eigenpy.git", branch: "devel"
 
   stable do
